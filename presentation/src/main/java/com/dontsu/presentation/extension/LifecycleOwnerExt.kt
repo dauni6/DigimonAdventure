83a88c184ad7b30d1208a38bcbfe6934e0ff40cc
dauni6 <45280927+dauni6@users.noreply.github.com>
2024-01-02 21:59:49 +0900

    refector : added lifecycle extension functions for Flow

--- presentation/src/main/java/com/dontsu/presentation/extension/LifecycleOwnerExt.kt
+++ presentation/src/main/java/com/dontsu/presentation/extension/LifecycleOwnerExt.kt
@@ -1,8 +1,16 @@
 package com.dontsu.presentation.extension
 
-import androidx.lifecycle.*
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 fun <T> LifecycleOwner.repeatOnStarted(
     flow: Flow<T>,
@@ -10,7 +18,7 @@ fun <T> LifecycleOwner.repeatOnStarted(
 ) {
     lifecycleScope.launch {
         repeatOnLifecycle(Lifecycle.State.STARTED) {
-            flow.collect {
+            flow.distinctUntilChanged().collect {
                 block(it)
             }
         }
@@ -22,8 +30,72 @@ fun <T> LifecycleOwner.flowWithStarted(
     block: suspend (result: T) -> Unit
 ) {
     lifecycleScope.launch {
-        flow.flowWithLifecycle(lifecycle, Lifecycle.State.STARTED).collect {
+        flow.flowWithLifecycle(lifecycle, Lifecycle.State.STARTED).distinctUntilChanged().collect {
             block(it)
+        }
+    }
+}
+
+/**
+ * To handle Event for UI based on Channel
+ * */
+fun <T> LifecycleOwner.observeAsEvents(
+    flow: Flow<T>,
+    block: (T) -> Unit
+) {
+    lifecycleScope.launch {
+        repeatOnLifecycle(Lifecycle.State.STARTED) {
+            withContext(Dispatchers.Main.immediate) {
+                flow.collect {
+                    block(it)
+                }
+            }
+        }
+    }
+}
+
+
+/**
+* @link https://developer.android.com/topic/libraries/architecture/coroutines?hl=ko#restart
+* */
+fun <T> Fragment.repeatOnStarted(
+    flow: Flow<T>,
+    block: suspend (result: T) -> Unit
+) {
+    viewLifecycleOwner.lifecycleScope.launch {
+        viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+            flow.distinctUntilChanged().collect {
+                block(it)
+            }
+        }
+    }
+}
+
+fun <T> Fragment.flowWithStarted(
+    flow: Flow<T>,
+    block: suspend (result: T) -> Unit
+) {
+    viewLifecycleOwner.lifecycleScope.launch {
+        flow.flowWithLifecycle(viewLifecycleOwner.lifecycle, Lifecycle.State.STARTED).distinctUntilChanged().collect {
+            block(it)
+        }
+    }
+}
+
+/**
+ * To handle Event for UI based on Channel
+ * */
+fun <T> Fragment.observeAsEvents(
+    flow: Flow<T>,
+    block: (T) -> Unit
+) {
+    viewLifecycleOwner.lifecycleScope.launch {
+        repeatOnLifecycle(Lifecycle.State.STARTED) {
+            withContext(Dispatchers.Main.immediate) {
+                flow.distinctUntilChanged().collect {
+                    block(it)
+                }
+            }
         }
     }
 }
